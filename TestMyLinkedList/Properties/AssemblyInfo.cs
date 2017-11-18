28d1ff008c80e69a23cd49a433678f6348a34854
AleksTor2208 <aleksander.toropowski@gmail.com>
2017-11-18 19:57:03 +0300

    Add TestMyLinkedList project

---
+++ TestMyLinkedList/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestMyLinkedList")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestMyLinkedList")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("672a1047-cea7-4ae9-abaa-d11c2db649c5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestMyLinkedList/TestMyLinkedList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyLinkedList;
+using System.Collections.Generic;
+
+namespace TestMyLinkedList
+{
+    [TestClass]
+    public class TestMyLinkedList
+    {
+        [TestMethod]
+        public void TestGetByIndexReturnValidValue()
+        {
+            var LinkedList = new MyLinkedList<int>();
+            LinkedList.Add(1);
+            LinkedList.Add(2);
+            var actual = LinkedList.GetByIndex(0);
+            var expected = 1;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestIndexOutOfRangeExceptionIsThrownWhenGetByIndexHasInvalidArgs()
+        {
+            var myList = new MyLinkedList<int>();           
+            Assert.ThrowsException<IndexOutOfRangeException>(() => myList.GetByIndex(1));
+        }
+
+        [TestMethod]
+        public void TestAsListMethodReturnValidType()
+        {
+            var myList = new MyLinkedList<string>();
+            myList.Add("example string");
+            var actual = myList.AsList().GetType();
+            var expected = new List<string>().GetType();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestAsListMethodReturnProperValue()
+        {
+            var myList = new MyLinkedList<string>();
+            myList.Add("No i cóż");
+            myList.Add("że ze");
+            myList.Add("Szwecji");
+            var expected = "Szwecji";
+            var actual = myList.AsList()[2];
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
